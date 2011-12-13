799142445089226220882a75b2c31ca647443c05
Stephane Paul <stephane.paul@gmail.com>
2011-12-13 01:08:50 -0500

    removed old versions of version, certificate and gemspec

--- lib/spree_coupons_gift_certificate.rb
+++
@@ -1,5 +0,0 @@
-require "spree_coupons_gift_certificate/version"
-
-module SpreeCouponsGiftCertificate
-  # Your code goes here...
-end
--- lib/spree_coupons_gift_certificate/version.rb
+++
@@ -1,3 +0,0 @@
-module SpreeCouponsGiftCertificate
-  VERSION = "0.0.1"
-end
